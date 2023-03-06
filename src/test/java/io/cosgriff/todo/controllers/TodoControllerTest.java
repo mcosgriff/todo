dcfad21385b6c872b4d0a8b11dac9a7648e98e71
Matthew J. Cosgriff <mcosgriff@gmail.com>
2023-03-05 17:46:09 -0700

    Added controller test

--- src/test/java/io/cosgriff/todo/controllers/TodoControllerTest.java
+++ src/test/java/io/cosgriff/todo/controllers/TodoControllerTest.java
@@ -4,26 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cosgriff.todo.models.Todo;
 import io.cosgriff.todo.services.TodoService;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,7 +31,7 @@ public class TodoControllerTest {
     private TodoService todoService;
     private Todo todo;
     private List<Todo> TodoList;
-    @InjectMocks
+    //@InjectMocks
     private TodoController todoController;
     @Autowired
     private MockMvc mockMvc;
@@ -44,6 +42,7 @@ public class TodoControllerTest {
     @BeforeEach
     public void setup() {
         todo = new Todo(1L, "ball", "670");
+        todoController = new TodoController(todoService);
         mockMvc = MockMvcBuilders.standaloneSetup(todoController).build();
     }
 
@@ -56,15 +55,12 @@ public class TodoControllerTest {
     public void PostMappingOfTodo() throws Exception {
         Mockito.when(todoService.save(Mockito.any(Todo.class))).thenReturn(todo);
 
-        MvcResult result = mockMvc.perform(post("/api/v1/todo").
+        mockMvc.perform(post("/api/v1/todo").
                         contentType(MediaType.APPLICATION_JSON).
                         content(mapper.writeValueAsString(todo)))
-                .andReturn();
-        //andExpect(status().isOk()).
-        //andExpect(jsonPath("$.title").value("ball"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("ball"));
 
-        Assertions.assertEquals(result.getResponse().getStatus(), HttpStatus.OK);
-
-        //Mockito.verify(todoService, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(todoService, Mockito.times(1)).save(Mockito.any(Todo.class));
     }
 }
